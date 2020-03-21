ba311a5da95a6449d8737b06033551eeb94e4e4c
Fallopio <kibaryshnikov@yandex.ru>
2020-03-21 16:24:50 +0400

    Updates

---
+++ precache-manifest.6c3f64879752b3f9edb9a88ae4c69231.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e721595122ec2062decd8c671a9cdd",
+    "url": "/BuildingConstructorApp/index.html"
+  },
+  {
+    "revision": "4a8255e0ee1f44b10018",
+    "url": "/BuildingConstructorApp/static/css/main.97f7f3cd.chunk.css"
+  },
+  {
+    "revision": "6480fc504d8df8984c15",
+    "url": "/BuildingConstructorApp/static/js/2.aa7633a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BuildingConstructorApp/static/js/2.aa7633a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a8255e0ee1f44b10018",
+    "url": "/BuildingConstructorApp/static/js/main.c1fbe82b.chunk.js"
+  },
+  {
+    "revision": "f4334db1796c183a6156",
+    "url": "/BuildingConstructorApp/static/js/runtime-main.097b4eec.js"
+  }
+]);
